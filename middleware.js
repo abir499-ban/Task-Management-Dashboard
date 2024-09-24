2f668c5117954fa2214ca037662338a4b6d4cb35
Abir Banerjee <abirbanerjee793@gmail.com>
2024-09-24 16:12:09 +0530

    Routes protected

--- middleware.js
+++ middleware.js
@@ -1,32 +1,45 @@
-import {NextResponse} from 'next/server'
+import { NextResponse } from 'next/server'
 
 export async function middleware(req) {
-    console.log("Middleware executed for '/' route");
-    const token = req.cookies.get("token");
-    if(!token){
-       return NextResponse.redirect(new URL('/login', req.url));
-    }
+    if (req.nextUrl.pathname == '/') {
+        console.log("Middleware executed for '/' route");
+        const token = req.cookies.get("token");
+        if (!token) {
+            return NextResponse.redirect(new URL('/login', req.url));
+        }
 
-    const {value} = token;
-    const uri = `${req.nextUrl.origin}/api/users/token`;
-    const res = await fetch(uri, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify( {value} ), 
-    });
-    const result = await res.json();
-    if(!result.success){
-        return NextResponse.redirect(new URL('/login', req.url));
+        const { value } = token;
+        const uri = `${req.nextUrl.origin}/api/users/token`;
+        const res = await fetch(uri, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ value }),
+        });
+        const result = await res.json();
+        if (!result.success) {
+            return NextResponse.redirect(new URL('/login', req.url));
+        }
+
+        const response = NextResponse.next();
+        response.cookies.set('user_email', result.user_email);
+        response.cookies.set('user_id', result.user_id);
+        return response;
+    }else if(req.nextUrl.pathname == '/sign-up'){
+
+        const token = req.cookies.get('token');
+        if(token) return NextResponse.redirect(new URL('/', req.url));
+        return NextResponse.next();
+
+    }else if(req.nextUrl.pathname == '/login'){
+        const token = req.cookies.get('token');
+        if(token) return NextResponse.redirect(new URL('/', req.url));
+        return NextResponse.next();
     }
-    
-    const response = NextResponse.next();
-    response.cookies.set('user_email', result.user_email);
-    response.cookies.set('user_id', result.user_id);
-    return response;
-  }
-  
-  export const config = {
-    matcher: '/', 
-  };
+}
+
+
+export const config = {
+    matcher: ['/', '/sign-up', '/login'],
+};
