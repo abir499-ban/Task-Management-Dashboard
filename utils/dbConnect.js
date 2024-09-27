cf924051b59b04e30584f85cce47bafe21b6c5a4
Abir Banerjee <abirbanerjee793@gmail.com>
2024-09-27 16:43:06 +0530

    Cache database connection

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,13 +1,36 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
 const uri = process.env.MONGO_DB_URL;
+
+if (!uri) {
+  throw new Error("No MongoDB URL found");
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 export async function connectToMongoDB() {
-    if (!uri) throw new Error("No MongoDB url");
-    try {
-       mongoose.connect(uri).then(()=> console.log("MongoDB connected succesfully"));
-    }
-    catch (error) {
-        console.log("Error occured ", error);
-        throw error;
-    }
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const options = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(uri, options).then((mongoose) => {
+      console.log("MongoDB connected successfully");
+      return mongoose;
+    }).catch((error) => {
+      console.log("MongoDB connection error:", error);
+      throw error;
+    });
+  }
+  
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
